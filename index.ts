12d634312e8751581866a6c3f4a25001decaeb7e
Sergyo97 <31071190+Sergyo97@users.noreply.github.com>
2023-01-29 22:38:24 -0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+import { EMPTY, fromEvent, of } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { catchError, concatMap, map } from 'rxjs/operators';
+
+const endpointInput: HTMLInputElement =
+  document.querySelector('input#endpoint');
+const fetchButton = document.querySelector('button#fetch');
+
+fromEvent(fetchButton, 'click')
+  .pipe(
+    map(() => endpointInput.value),
+    concatMap((value) =>
+      ajax(`https://random-data-api.com/api/v2/${value}`).pipe(
+        catchError((error) => of(`Could not fetch data: ${error}`))
+      )
+    )
+  )
+  .subscribe({
+    next: (value) => console.log(value),
+    error: (err) => console.log('Error', err),
+    complete: () => console.log('Completed'),
+  });
